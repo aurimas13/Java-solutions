3ef0aaf5c5ca85392d6226970c9b22bc4d28bff8
aurimas13 <aurimas.nausedas@gmail.com>
2020-11-25 16:55:54 +0200

    Add files via upload

---
+++ BiNumber3Runner.java
@@ -0,0 +1,14 @@
+package com.in28minutes.oops;
+
+public class BiNumber3Runner {
+
+	public static void main(String[] args) {
+		BiNumber3 numbers3 = new BiNumber3(2,3);
+		System.out.println(numbers3.add());
+		System.out.println(numbers3.multiply());
+		numbers3.doubleValue();
+		System.out.println(numbers3.getNumber3());
+		System.out.println(numbers3.getNumber4());
+	}
+
+}
