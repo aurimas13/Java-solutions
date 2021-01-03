1e6489cc2096f1b7d01f5f41a63461e9f756fc82
aurimas13 <aurimas.nausedas@gmail.com>
2021-01-03 17:24:52 +0200

    Solution to Array problem

---
+++ StudentsCollectionRunner.java
@@ -0,0 +1,39 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class AscendingStudentComparator implements Comparator<Student> {
+
+	@Override
+	public int compare(Student student1, Student student2) {
+		return Integer.compare(student1.getId(), student2.getId());
+	}
+
+}
+
+public class StudentsCollectionRunner {
+
+	public static void main(String[] args) {
+		List<Student> students = List.of(new Student(1, "Aurimas"),
+			new Student(100, "John"),
+				new Student(2, "Emma"));
+
+		List<Student> studentsAI = new ArrayList<>(students);
+
+		System.out.println(students);
+
+		Collections.sort(studentsAI);
+		System.out.println("Descending" + studentsAI);
+
+		Collections.sort(studentsAI, new AscendingStudentComparator());
+		System.out.println("AscendingStudentComparator" + studentsAI);
+		
+		studentsAI.sort(new AscendingStudentComparator());
+		System.out.println("AscendingStudentComparator" + studentsAI);
+
+	}
+
+}
