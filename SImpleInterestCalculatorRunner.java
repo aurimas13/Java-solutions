5fb395498528a022068f2d75afb12977df346185
Aurimas A. Nausėdas <aurimas.nausedas@gmail.com>
2021-11-07 07:38:12 +0200

    Update SImpleInterestCalculatorRunner.java

--- SImpleInterestCalculatorRunner.java
+++ SImpleInterestCalculatorRunner.java
@@ -1,4 +1,9 @@
-package aurimas.oops; // Solution of Object-Oriented Programming and BigDecimal problem
+// Solution of Object-Oriented Programming and BigDecimal problem
+//
+// Created by Aurimas A. Nausedas on 05/11/20.
+// Updated by Aurimas A. Nausedas on 07/11/21.
+
+package aurimas.oops;
 
 import java.math.BigDecimal;
 
